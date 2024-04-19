d9b7e3a37db8112ee84e455a1256bc6f5be0cff7
Jatin Kandpal <jxk217@student.bham.ac.uk>
2024-04-20 00:00:43 +0100

    Added new classes for creating and reading individual activity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,9 +1,6 @@
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
@@ -19,7 +16,15 @@ namespace API.Controllers
         [HttpGet("{id}")]  //api/activities/sghshs
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
+            return await Mediator.Send(new Details.Query{Id = id});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            await Mediator.Send(new Create.Command {Activity = activity});
             return Ok();
         }
+
     }
 }
