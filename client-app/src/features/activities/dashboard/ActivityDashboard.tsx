7419e04c3a3fedcb8feaca8914642ac2c5add73f
Jatin Kandpal <jxk217@student.bham.ac.uk>
2024-05-03 17:48:40 +0100

    Used Semantic UI react to improve on the interface

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,6 +4,7 @@ import ActivityList from './ActivityList';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
 import LoadingComponent from '../../../app/layout/LoadingComponents';
+import ActivityFilters from './ActivityFilters';
 
 
 export default observer(function ActivityDashboard() {
@@ -23,7 +24,7 @@ export default observer(function ActivityDashboard() {
                 <ActivityList />
             </Grid.Column>
             <GridColumn width='6'>
-                <h2>Activity filters</h2>
+                <ActivityFilters/>
             </GridColumn>
         </Grid>
     )
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import 'semantic-ui-css/semantic.min.css'
-import './app/layout/styles.css'
-import { StoreContext, store } from './app/stores/store'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './app/router/Routes'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'semantic-ui-css/semantic.min.css';
+import 'react-calendar/dist/Calendar.css';
+import './app/layout/styles.css';
+import { StoreContext, store } from './app/stores/store';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/Routes';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
