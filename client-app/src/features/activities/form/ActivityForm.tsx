cf90d3f7e623a93ce90514f6563cc8e6bf6aab94
Jatin Kandpal <jxk217@student.bham.ac.uk>
2024-05-12 14:13:18 +0100

    writing reuable code for text input and text area

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, useEffect, useState } from 'react';
-import { Button, FormField, Label, Segment } from 'semantic-ui-react';
+import { Button, Label, Segment } from 'semantic-ui-react';
 
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
@@ -7,9 +7,13 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 import { Activity } from '../../../app/models/activity';
 import LoadingComponent from '../../../app/layout/LoadingComponents';
 import { v4 as uuid } from 'uuid';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, ErrorMessage } from 'formik';
 import { values } from 'mobx';
 import * as Yup from 'yup';
+import MyTextInput from '../../../app/common/form/MyTextInput';
+import MyTextArea from '../../../app/common/form/MyTextArea';
+import MySelectInput from '../../../app/common/form/MySelectInput';
+import { categoryOptions } from '../../../app/common/options/categoryOptions';
 
 
 export default observer(function ActivityForms() {
@@ -31,7 +35,12 @@ export default observer(function ActivityForms() {
 
 
     const validationSchema = Yup.object({
-        title: Yup.string().required('the activity title is required')
+        title: Yup.string().required('the activity title is required'),
+        description: Yup.string().required('the activity description is required'),
+        category: Yup.string().required(),
+        date: Yup.string().required(),
+        venue: Yup.string().required(),
+        city: Yup.string().required(),
     })
     useEffect(() => {
         if (id) loadActivity(id).then(activity => setActivity(activity!))
@@ -47,7 +56,7 @@ export default observer(function ActivityForms() {
     //     }
     // }
 
-    // function handleChange(event: ChangeEventFieldElement | HTMLTextAreaElement>) {
+    // function handleChange(event: ChangeEventMyTextInputElement | HTMLTextAreaElement>) {
     //     const { name, value } = event.target;
     //     setActivity({ ...activity, [name]: value })
     // }
@@ -63,17 +72,12 @@ export default observer(function ActivityForms() {
                 onSubmit={values => console.log(values)} >
                 {({ handleSubmit }) => (
                     <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-                        <FormField>
-                            <Field placeholder='Title' name='title' />
-                            <ErrorMessage name='title'
-                                render={error => <Label basic color='red' content={error} />} />
-                        </FormField>
-
-                        <Field placeholder='Description' name='description' />
-                        <Field placeholder='Category' name='category' />
-                        <Field type='date' placeholder='Date' name='date' />
-                        <Field placeholder='City' value={activity.city} name='city' />
-                        <Field placeholder='Venue' name='venue' />
+                        <MyTextInput name = 'title' placeholder='Title'/>
+                        <MyTextArea rows={3} placeholder='Description' name='description' />
+                        <MySelectInput options={categoryOptions} placeholder='Category' name='category' />
+                        <MyTextInput placeholder='Date' name='date' />
+                        <MyTextInput placeholder='City' name='city' />
+                        <MyTextInput placeholder='Venue' name='venue' />
                         <Button loading={loading} floated='right' positive type='submit' content='Submit' />
                         <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
                     </Form>
