a88ffd4cc4a73abe3f85dba2d60ac0ebcdcf56af
Jatin Kandpal <jxk217@student.bham.ac.uk>
2024-05-14 11:24:53 +0100

    created DTOs

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,10 @@
+namespace API.DTOs
+{
+    public class UserDtos
+    {
+        public string DisplayName { get; set; }
+        public string Token { get; set; }
+        public string Image { get; set; }
+        public string  Username { get; set; }
+    }
+}
