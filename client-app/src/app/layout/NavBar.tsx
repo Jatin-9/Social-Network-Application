908fa07b545100241e6ab524803b1a4fd9cf7467
Jatin Kandpal <jxk217@student.bham.ac.uk>
2024-05-19 15:28:58 +0100

    updated homePage and nav bar

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,8 +1,10 @@
-import { Button, Container, Menu } from 'semantic-ui-react';
-import { NavLink } from 'react-router-dom';
+import { Button, Container, Menu, MenuItem, Image, Dropdown, DropdownItem, DropdownMenu } from 'semantic-ui-react';
+import { Link, NavLink } from 'react-router-dom';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
-export default function NavBar() {
-
+export default observer(function NavBar() {
+    const { userStore: { user, logout } } = useStore();
     return (
         <Menu inverted fixed='top'>
             <Container>
@@ -15,7 +17,16 @@ export default function NavBar() {
                 <Menu.Item >
                     <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
                 </Menu.Item>
+                <MenuItem position='right'>
+                    <Image src={user?.image || '/assets/user.png'} avatar spaced='right' />
+                    <Dropdown pointing='top left' text={user?.displayName} >
+                        <DropdownMenu>
+                            <DropdownItem as={Link} to={`/profile/${user?.username}`} text='My Profile' icon='user' />
+                            <DropdownItem onClick={logout} text='Logout' icon='power' />
+                        </DropdownMenu>
+                    </Dropdown>
+                </MenuItem>
             </Container>
         </Menu>
     )
-}
+})
