d8d38e5040f4e4893b734504356796f49fd70afc
Jatin Kandpal <jxk217@student.bham.ac.uk>
2024-05-24 19:55:59 +0100

    added attendance handler

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -11,7 +11,10 @@ namespace Application.Activities
         public string Category { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
+        
         public string HostUsername { get; set; }
+        public Boolean IsCancelled { get; set; }
+
         public ICollection<Profile> Attendees { get; set; }
         
     }
