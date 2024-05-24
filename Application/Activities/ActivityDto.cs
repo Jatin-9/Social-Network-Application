12a05142b6eba825453d77ca016bf8747a32f398
Jatin Kandpal <jxk217@student.bham.ac.uk>
2024-05-24 18:15:09 +0100

    added new project to configure new realtion between entities

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -1,17 +1,18 @@
-using System;
+using Application.Profiles;
 
-namespace Domain
+namespace Application.Activities
 {
-    public class Activity
+    public class ActivityDto
     {
-        public Guid Id { get; set; }
+         public Guid Id { get; set; }
         public string Title { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
-        public ICollection<ActivityAttendee> Attendees { get; set; } = new List<ActivityAttendee>();
-
+        public string HostUsername { get; set; }
+        public ICollection<Profile> Attendees { get; set; }
+        
     }
 }
--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,7 +1,9 @@
 using Application.Core;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -10,32 +12,45 @@ namespace Application.Activities
     {
         public class Command : IRequest<Result<Unit>>
         {
-            public Activity Activity {get; set;}
+            public Activity Activity { get; set; }
         }
 
 
-           public class CommandValidator  :  AbstractValidator<Command>
+        public class CommandValidator : AbstractValidator<Command>
         {
-            public CommandValidator() 
+            public CommandValidator()
             {
                 RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
             }
         }
         public class Handler : IRequestHandler<Command, Result<Unit>>
         {
-        private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
-            _context = context;
+                _userAccessor = userAccessor;
+                _context = context;
             }
 
             public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
+                var user = await _context.Users.FirstOrDefaultAsync(x =>
+                x.UserName == _userAccessor.GetUsername());
+
+                var attendee = new ActivityAttendee
+                {
+                    AppUser = user,
+                    Activity = request.Activity,
+                    IsHost = true
+                };
+                request.Activity.Attendees.Add(attendee);
+                
                 _context.Activities.Add(request.Activity);
 
                 var result = await _context.SaveChangesAsync() > 0;
-                if(!result) return Result<Unit>.Failure("Failed to create activity");
-                
+                if (!result) return Result<Unit>.Failure("Failed to create activity");
+
                 return Result<Unit>.Success(Unit.Value);
             }
         }
