81a9b8489e6eeafbfcad645af120437620da995c
Jatin Kandpal <jxk217@student.bham.ac.uk>
2024-05-27 19:11:06 +0100

    added photo upload logic and handler

--- Application/Interfaces/IPhotoAccessor.cs
+++ Application/Interfaces/IPhotoAccessor.cs
@@ -10,6 +10,6 @@ namespace Application.Interfaces
     public interface IPhotoAccessor
     {
         Task<PhotoUploadResult> AddPhoto(IFormFile file);
-        Task<string> DeletePhotot(string PublicId);
+        Task<string> DeletePhoto(string publicId);
     }
 }
