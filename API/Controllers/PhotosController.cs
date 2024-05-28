168f3b58c998bafc38388096eba86f7441a195d7
Jatin Kandpal <jxk217@student.bham.ac.uk>
2024-05-28 15:18:41 +0100

    making logic for deleting photo and setting main photo logic

--- API/Controllers/PhotosController.cs
+++ API/Controllers/PhotosController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Photos;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -13,6 +14,19 @@ namespace API.Controllers
         public async Task<IActionResult> Add([FromForm] Add.Command command)
         {
             return HandleResult(await Mediator.Send(command));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            return HandleResult(await Mediator.Send(new Delete.Command{Id = id}));
+        }
+
+        [HttpPost("{id}/setMain")]
+
+        public async Task<IActionResult> SetMain(string id)
+        {
+            return HandleResult(await Mediator.Send(new SetMain.Command{Id = id}));
         }
     }
 }
