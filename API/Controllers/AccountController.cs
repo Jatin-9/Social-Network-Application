9698457a030ff69c1132f27cd705df5214cce264
Jatin Kandpal <jxk217@student.bham.ac.uk>
2024-05-30 21:49:44 +0100

    updated mapping config and added profile section for user

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -26,7 +26,8 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            var user = await _userManager.Users.Include(p=> p.Photos)
+            .FirstOrDefaultAsync(x=> x.Email == loginDto.Email);
             if (user == null) return Unauthorized();
             var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
             if (result)
@@ -73,7 +74,8 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<UserDto>> GetCurrentUser()
         {
-            var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+            var user = await _userManager.Users.Include(p=> p.Photos)
+            .FirstOrDefaultAsync(x=> x.Email == User.FindFirstValue(ClaimTypes.Email));
             return CreateUserObject(user);
         }
          private UserDto CreateUserObject(AppUser user)
@@ -81,7 +83,7 @@ namespace API.Controllers
             return new UserDto
             {
                 DisplayName = user.DisplayName,
-                Image = null,
+                Image = user?.Photos?.FirstOrDefault(x=> x.IsMain)?.Url,
                 Token = _tokenService.CreateToken(user),
                 Username = user.UserName
             };
--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -15,7 +15,7 @@ namespace Application.Activities
         public string HostUsername { get; set; }
         public Boolean IsCancelled { get; set; }
 
-        public ICollection<Profile> Attendees { get; set; }
+        public ICollection<AttendeeDto> Attendees { get; set; }
         
     }
 }
