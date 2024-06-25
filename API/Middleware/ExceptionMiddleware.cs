f38eaa693ac74f11526c006de5915d3e78396736
Jatin Kandpal <jxk217@student.bham.ac.uk>
2024-06-25 14:53:08 +0100

    minor update

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -33,7 +33,7 @@ namespace API.Middleware
 
                 var response = _env.IsDevelopment()
                 ? new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
-                : new AppException(context.Response.StatusCode, "Internal Server Error");
+                : new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace);
 
                 var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
 
