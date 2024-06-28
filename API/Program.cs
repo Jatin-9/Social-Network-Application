e9bcfe3d6e07b7aab1a029fd41f1312d296d95d0
Jatin Kandpal <jxk217@student.bham.ac.uk>
2024-06-28 22:04:37 +0100

    add to append changes

--- API/Program.cs
+++ API/Program.cs
@@ -47,7 +47,7 @@ else
 {
     app.Use(async (context, next) =>
     {
-        context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+        context.Response.Headers.Append("Strict-Transport-Security", "max-age=31536000");
         await next.Invoke();
     });
 }
